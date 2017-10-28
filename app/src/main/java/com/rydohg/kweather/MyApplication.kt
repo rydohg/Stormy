d9606778f66b254025bdf7fd544c91af1b47979d
Ryan <rdoh62001@gmail.com>
2017-10-28 13:11:52 -0400

    Change image depending on conditions

---
+++ app/src/main/java/com/rydohg/kweather/MyApplication.kt
@@ -0,0 +1,24 @@
+package com.rydohg.kweather
+
+import android.app.Application
+import android.content.Context
+
+class MyApplication : Application() {
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance: MyApplication? = null
+
+        fun applicationContext() : Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val context: Context = MyApplication.applicationContext()
+    }
+}
--- app/src/main/java/com/rydohg/kweather/WeatherAdapter.kt
+++ app/src/main/java/com/rydohg/kweather/WeatherAdapter.kt
@@ -1,11 +1,15 @@
 package com.rydohg.kweather
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import android.view.View
+import android.widget.ImageView
 
 
 class WeatherAdapter constructor(private val forecastList: ArrayList<Forecast>): RecyclerView.Adapter<WeatherAdapter.CustomViewHolder>(){
@@ -29,6 +33,24 @@ class WeatherAdapter constructor(private val forecastList: ArrayList<Forecast>):
         holder.lowTempTextView.text = lowTempString
         holder.desc.text = desc
         holder.date.text = date
+        holder.image.setImageDrawable(imageFromDesc(desc))
+    }
+
+    private fun imageFromDesc(desc: String): Drawable? {
+        val context = MyApplication.applicationContext()
+        return when (desc) {
+            "Sun" -> drawableForId(R.drawable.sun, context)
+            "Rain" -> drawableForId(R.drawable.rain, context)
+            else -> drawableForId(R.drawable.sun, context)
+        }
+    }
+
+    private fun drawableForId(id: Int, context: Context): Drawable {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            context.resources.getDrawable(id, context.theme)
+        } else {
+            context.resources.getDrawable(id)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -40,6 +62,7 @@ class WeatherAdapter constructor(private val forecastList: ArrayList<Forecast>):
         var lowTempTextView: TextView = rootView.findViewById(R.id.low_temp_text_view)
         var desc: TextView = rootView.findViewById(R.id.desc_text_view)
         var date: TextView = rootView.findViewById(R.id.date_text_view)
+        var image: ImageView = rootView.findViewById(R.id.weather_image_view)
     }
 
 }

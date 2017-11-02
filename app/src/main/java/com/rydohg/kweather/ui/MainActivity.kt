f69ef74a9b2c828f7116388ba8d2e12e75ea8453
Ryan School <rdoh62001@gmail.com>
2017-11-02 10:35:17 -0400

    Don't crash when disconnected from internet

--- app/src/main/java/com/rydohg/kweather/ui/MainActivity.kt
+++ app/src/main/java/com/rydohg/kweather/ui/MainActivity.kt
@@ -1,25 +1,25 @@
 package com.rydohg.kweather.ui
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.AsyncTask
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import android.support.v7.widget.DividerItemDecoration
-import android.widget.ImageView
-import android.widget.TextView
-import com.rydohg.kweather.utils.JsonParser
-import com.rydohg.kweather.R
-import com.rydohg.kweather.utils.imageFromDesc
-import com.rydohg.kweather.utils.kelvinToFahrenheit
 import android.view.Menu
 import android.view.MenuItem
-import android.content.Intent
+import android.widget.ImageView
+import android.widget.TextView
+import com.rydohg.kweather.R
 import com.rydohg.kweather.SettingsActivity
+import com.rydohg.kweather.utils.JsonParser
+import com.rydohg.kweather.utils.imageFromDesc
+import com.rydohg.kweather.utils.kelvinToFahrenheit
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 
 class MainActivity : AppCompatActivity() {
@@ -38,7 +38,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle item selection
         return when (item.itemId) {
             R.id.settings_item -> {
                 val intent = Intent(this, SettingsActivity::class.java)
@@ -52,13 +51,18 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("StaticFieldLeak")
     inner class NetworkAsyncTask : AsyncTask<Void, Void, String>() {
         override fun doInBackground(vararg p0: Void?): String {
-            val client = OkHttpClient()
-            val request = Request.Builder()
-                    .url("http://api.openweathermap.org/data/2.5/forecast/daily?zip=32904,us&appid=829aad9b0941ffc8fbb7a9d2ad9c334c")
-                    .build()
-            val response = client.newCall(request).execute()
+            try {
+                val client = OkHttpClient()
+                val request = Request.Builder()
+                        .url("http://api.openweathermap.org/data/2.5/forecast/daily?zip=32904,us&appid=829aad9b0941ffc8fbb7a9d2ad9c334c")
+                        .build()
+                val response = client.newCall(request).execute()
 
-            return response.body()!!.string()
+                return response.body()!!.string()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return ""
         }
 
         override fun onPostExecute(result: String?) {

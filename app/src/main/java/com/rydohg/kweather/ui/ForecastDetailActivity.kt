a4a4da47fe0362c56cd4007d35dc712c2d52f6a4
Ryan School <rdoh62001@gmail.com>
2017-11-21 11:10:02 -0500

    Start design of ForecastDetail Activity

--- app/src/main/java/com/rydohg/kweather/ui/ForecastDetailActivity.kt
+++ app/src/main/java/com/rydohg/kweather/ui/ForecastDetailActivity.kt
@@ -2,6 +2,7 @@ package com.rydohg.kweather.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import com.rydohg.kweather.R
 import com.rydohg.kweather.utils.Forecast
 
@@ -13,5 +14,13 @@ class ForecastDetailActivity : AppCompatActivity() {
 
         val intent = this.intent
         val forecast = intent.extras.getSerializable("forecast") as Forecast
+
+        val maxTextView: TextView = findViewById(R.id.max_text_view)
+        val minTextView: TextView = findViewById(R.id.min_text_view)
+        val precipTextView: TextView = findViewById(R.id.precip_text_view)
+
+        maxTextView.text = forecast.maxTempCelsius.toInt().toString()
+        minTextView.text = forecast.minTempCelsius.toInt().toString()
+        precipTextView.text = "Chance of " + forecast.precipType.capitalize() + ": " + (forecast.precipProbability * 100).toInt().toString() + "%"
     }
 }
